32c74bfda08d2b069f4df33eea5bee3fdf41805e
AgustinRiple <agusriple@hotmail.com>
2026-04-18 18:13:03 -0300

    Agregar middleware de whitenoise

--- sistema_bienes/settings/base.py
+++ sistema_bienes/settings/base.py
@@ -30,6 +30,7 @@ AUTH_USER_MODEL = 'core.Usuario'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
