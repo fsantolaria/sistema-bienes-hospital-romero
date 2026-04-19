90eef69966e8d78a214fe39f1096b5513a96f0f6
AgustinRiple <agusriple@hotmail.com>
2026-04-19 18:29:32 -0300

    Corregir settings module a settings.development para entorno local

--- sistema_bienes/wsgi.py
+++ sistema_bienes/wsgi.py
@@ -1,6 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_bienes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_bienes.settings.development')
+
 
 application = get_wsgi_application()
