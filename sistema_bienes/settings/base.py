8f30715c40b7f3a4d87e4c16066aad9fa83a0ece
joaquinrc26 <joaquinramoscristeche@gmail.com>
2026-04-19 12:25:32 -0300

    Agregar middleware NoCacheMiddleware para prevenir cierre de sesión al usar back button

--- sistema_bienes/settings/base.py
+++ sistema_bienes/settings/base.py
@@ -37,6 +37,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core.middleware.NoCacheMiddleware",
 ]
 
 ROOT_URLCONF = 'sistema_bienes.urls'
