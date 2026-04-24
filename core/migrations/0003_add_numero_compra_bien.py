a3687888993904426d4575fe5e84013f63986bf5
lucaskiernicki <ruizlucas421@gmail.com>
2026-04-24 20:39:09 -0300

    chore(migrations): add numero_compra to BienPatrimonial

---
+++ core/migrations/0003_add_numero_compra_bien.py
@@ -0,0 +1,17 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0002_notificacion_eliminada_operador_dni_and_more"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="bienpatrimonial",
+            name="numero_compra",
+            field=models.CharField(max_length=50, blank=True, default="", verbose_name="N° de Compra"),
+            preserve_default=False,
+        ),
+    ]
